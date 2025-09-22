c05638fecdc0e2d392a0807e67a8688f48e623b0
bkoimett <benkoimet@gmail.com>
2025-09-22 13:40:16 +0300

    9. Cookies Primer feat: install cookieparser, use to create cookies using res.cookie and read the cookies.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/authRoutes');
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -27,3 +29,23 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
+  // res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, secure: true});
+
+  res.send('you got the cookies!');
+
+});
+
+app.get('/read-cookies', (req,res) => {
+
+  const cookies = req.cookies;
+  console.log(cookies);
+
+  res.json(cookies);
+})
